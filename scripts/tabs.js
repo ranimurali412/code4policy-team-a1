2e0d90aa234af389c0355dd636c5fac2533f911d
Rani Murali <rmurali@hks.harvard.edu>
2021-01-18 11:49:51 -0500

    adding documentation for unused js file

--- scripts/tabs.js
+++ scripts/tabs.js
@@ -1,6 +1,6 @@
 // NOTE: This file is no longer used because we opted for anoter method (creating a navigation bar at the top of the webpage using HTML and CSS)
 
-function openCity(evt, cityName) {
+function openTab(evt, tabName) {
   // Declare all variables
   var i, tabcontent, tablinks;
 
@@ -17,6 +17,6 @@ function openCity(evt, cityName) {
   }
 
   // Show the current tab, and add an "active" class to the button that opened the tab
-  document.getElementById(cityName).style.display = "block";
+  document.getElementById(tabName).style.display = "block";
   evt.currentTarget.className += " active";
 }
